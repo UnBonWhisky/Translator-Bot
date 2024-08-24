d4197a7093c9590ada25eead7e813587c28e5261
UnBonWhisky <65244389+UnBonWhisky@users.noreply.github.com>
2024-08-24 21:25:10 +0000

    fixed error for messages > 2k chars

--- cogs/message_commands.py
+++ cogs/message_commands.py
@@ -8,6 +8,24 @@ import discord
 class MessageCommands(Cog):
     def __init__(self, bot):
         self.bot = bot
+        
+    async def split_message_into_parts(self, message, max_length=2000):
+        parts = []
+        while len(message) > max_length:
+            # Trouver la position du dernier espace avant la limite max_length
+            last_space = message.rfind(' ', 0, max_length)
+            if last_space != -1:
+                # Couper le message au dernier espace
+                parts.append(message[:last_space])
+                message = message[last_space+1:]
+            else:
+                # Si aucun espace n'est trouvé, couper au caractère max_length
+                parts.append(message[:max_length])
+                message = message[max_length:]
+        # Ajouter le reste du message s'il en reste
+        if message:
+            parts.append(message)
+        return parts
     
     ########################
     # Translate to english #
@@ -34,7 +52,13 @@ class MessageCommands(Cog):
             except :
                 await ctx.respond("An error occurred.")
                 return
-        await ctx.respond(f"{trad.text}", ephemeral=True)
+            
+        if len(trad.text) > 2000:
+            parts = await self.split_message_into_parts(trad.text)
+            for part in parts:
+                await ctx.respond(f"{part}", ephemeral=True)
+        else :
+            await ctx.respond(f"{trad.text}", ephemeral=True)
     
     
     #######################
@@ -62,7 +86,13 @@ class MessageCommands(Cog):
             except :
                 await ctx.respond("An error occurred.")
                 return
-        await ctx.respond(f"{trad.text}", ephemeral=True)
+        
+        if len(trad.text) > 2000:
+            parts = await self.split_message_into_parts(trad.text)
+            for part in parts:
+                await ctx.respond(f"{part}", ephemeral=True)
+        else :
+            await ctx.respond(f"{trad.text}", ephemeral=True)
         
     
     ########################
@@ -90,7 +120,13 @@ class MessageCommands(Cog):
             except :
                 await ctx.respond("An error occurred.")
                 return
-        await ctx.respond(f"{trad.text}", ephemeral=True)
+        
+        if len(trad.text) > 2000:
+            parts = await self.split_message_into_parts(trad.text)
+            for part in parts:
+                await ctx.respond(f"{part}", ephemeral=True)
+        else :
+            await ctx.respond(f"{trad.text}", ephemeral=True)
     
     #######################
     # Translate to custom #
@@ -123,7 +159,15 @@ class MessageCommands(Cog):
             description = f"Actually, I am doing the translation based on your discord client language setting.\nIt will be possible to change it manually in the future.",
             color = 0x5865F2
         )
-        await ctx.respond(f"{trad.text}", embed=EmbedMessage, ephemeral=True)
+        if len(trad.text) > 2000:
+            parts = await self.split_message_into_parts(trad.text)
+            for part in parts:
+                if part == parts[-1]:
+                    await ctx.respond(f"{part}", embed=EmbedMessage, ephemeral=True)
+                else :
+                    await ctx.respond(f"{part}", ephemeral=True)
+        else :
+            await ctx.respond(f"{trad.text}", embed=EmbedMessage, ephemeral=True)
 
 def setup(bot):
     print("Message Commands are ready !")
